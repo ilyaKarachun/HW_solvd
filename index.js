e3e2d5c5deef8abe5b18d6714fa27997e2cc5337
Ilya_Karachun <Karachun_ilya94@mail.ru>
2023-01-23 23:18:57 +0300

    feat: first task

---
+++ index.js
@@ -0,0 +1,33 @@
+let bigInt = require("big-integer");
+
+class Stringer  {
+
+    constructor(startStr) {
+        this.startStr = bigInt(startStr)
+    }
+
+    plus(str) {
+        let plusNumberStr = this.startStr.add(str)
+        return plusNumberStr.toString()
+    }
+    minus(str) {
+        let minusNumberStr = this.startStr.minus(str)
+        return minusNumberStr.toString()
+    }
+    divide(str) {
+        let divideNumberStr = this.startStr.divide(str)
+        return divideNumberStr.toString()
+    }
+    multiple(str) {
+        let multipleNumberStr = this.startStr.multiply(str)
+        return multipleNumberStr.toString()
+    }
+}
+
+let test = new Stringer('10000000000000000')
+
+console.log(test.plus(97328473487488383))
+console.log(test.minus(1))
+console.log(test.divide(2))
+console.log(test.multiple(97328473487488383))
+console.log(typeof (test.multiple(2)))
