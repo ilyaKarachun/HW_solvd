9c69846457926da93f1a11b1c803157a296226b5
Ilya_Karachun <Karachun_ilya94@mail.ru>
2023-02-21 18:17:55 +0300

    feat: finished

--- twoArrAsStack.js
+++ twoArrAsStack.js
@@ -1,44 +1,78 @@
+// class Stack {
+//     constructor() {
+//         this.queue1 = [];
+//         this.queue2 = [];
+//     }
+//     push(val) {
+//         this.queue1.push(val);
+//         while (this.queue2.length > 0) {
+//             this.queue1.push(this.queue2.shift());
+//         }
+//         [this.queue1, this.queue2] = [this.queue2, this.queue1];
+//     }
+//     pop() {
+//         return this.queue2.shift();
+//     }
+// }
+
 class Stack {
     constructor() {
-        this.queue1 = [];
-        this.queue2 = [];
+        this.arr = []
     }
-    push(val) {
-        this.queue1.push(val);
-        while (this.queue2.length > 0) {
-            this.queue1.push(this.queue2.shift());
-        }
-        [this.queue1, this.queue2] = [this.queue2, this.queue1];
+    top() {
+        return this.arr[this.arr.length - 1]
+    }
+    push(...val) {
+        return this.arr.push(...val)
     }
     pop() {
-        return this.queue2.shift();
+        return this.arr.pop()
+    }
+    isEmpty() {
+        return this.arr.length === 0
     }
 }
 
-class ArrByStack {
+let stackTest = new Stack()
+console.log(stackTest.isEmpty())
+stackTest.push(10,12,14,8)
+console.log(stackTest.isEmpty())
+console.log(stackTest.top())
+console.log(stackTest.pop())
+console.log(stackTest.pop())
+console.log(stackTest.pop())
+console.log(stackTest.pop())
+
+class Queue {
     constructor() {
-        this.stack1 = [];
-        this.stack2 = [];
+        this.stack1 = new Stack();
+        this.stack2 = new Stack();
     }
-    enqueue(value) {
-        this.stack1.push(value);
+    enqueue(...value) {
+        this.stack1.arr.push(...value);
     }
     dequeue() {
-        if (this.stack2.length === 0) {
-            while (this.stack1.length > 0) {
-                this.stack2.push(this.stack1.pop());
+        if (this.stack2.arr.length === 0) {
+            while (this.stack1.arr.length > 0) {
+                this.stack2.arr.push(this.stack1.arr.pop());
             }
         }
-        return this.stack2.pop();
+        return this.stack2.arr.pop();
+    }
+    isEmpty() {
+        return this.stack1.arr.length === 0 && this.stack2.arr.length === 0;
     }
 }
 
-/* In this implementation, we use two arrays or "stacks":
- inbox and outbox. The enqueue method simply pushes a value
-  onto the inbox stack. The dequeue method first checks if
-   the outbox stack is empty. If it is, it pops all the values
-    from the inbox stack and pushes them onto the outbox stack in
-     reverse order. This effectively reverses the order of the
-      elements so that the oldest element is now at the top of
-       the outbox stack. The method then pops the top value from
-        the outbox stack and returns it. */
+let test = new Queue()
+console.log(test.isEmpty())
+test.enqueue(3,4,1,10,20,40)
+console.log(test.isEmpty())
+test.enqueue(2)
+test.enqueue(3)
+test.enqueue(4)
+console.log(test)
+console.log(test.dequeue())
+console.log(test.dequeue())
+
+
